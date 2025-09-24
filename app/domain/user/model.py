dd58b413b51f418942b502d048f468ce526d6d58
Felipe Venas <felipevenas3003@gmail.com>
2025-09-24 13:29:03 -0300

    Overwriting: Resolvendo bugs do Git

--- app/domain/user/model.py
+++ app/domain/user/model.py
@@ -1,5 +1,11 @@
-class User():
-    def __init__(self, name, login, password):
-        self.name = name
-        self.login = login
-        self.password = password
+from app.db.config import db
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(70), nullable=False)
+    login = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(15), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' %self.name
