16769b14ca3a0a802c4014b2549a32215212449c
Felipe Venas <felipevenas3003@gmail.com>
2025-09-26 10:38:22 -0300

    Teste

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,6 +1,8 @@
 from flask import render_template, Blueprint, session, redirect, url_for
 
 from app.domain.music.model import Music
+from app.domain.upload.services import search_image
+from app.forms.forms import FormMusic, FormLogin, FormRegister
 
 # Esse controlador serve apenas para renderizar as páginas:
 
@@ -11,8 +13,8 @@ def index():
     return render_template("index.html",
                            title = "Início")
 
-@index_bp.route('/register')
-def register_page():
+@index_bp.route('/add')
+def add_page():
     if session['username'] == None or 'username' not in session:
         return redirect(url_for('index_bp.login_page'))
     return render_template("add_music.html", 
@@ -24,6 +26,12 @@ def update_page():
         return redirect(url_for('index_bp.login_page'))
     return render_template("edit_music.html",
                            title = "Editar música")
+    
+    form = FormMusic()
+    
+    return render_template("add_page.html", 
+                           title = "Cadastrar música",
+                           form=form)
 
 @index_bp.route('/musics')
 def list_page():
@@ -36,8 +44,38 @@ def list_page():
                            musics = lista,
                            title = "Lista de músicas")
 
+@index_bp.route('/update/<int:id>')
+def update_page(id):
+    if session['username'] == None or 'username' not in session:
+        flash("É necessário se autenticar!")
+        return redirect(url_for('index_bp.login_page'))
+    
+    find_music = Music.query.filter_by(id=id).first()
+
+    form = FormMusic()
+
+    form.name.data = find_music.name
+    form.artist.data = find_music.artist
+    form.genre.data = find_music.genre
+
+    album = search_image(id)
+
+    return render_template('update_page.html', 
+                           title = "Editar música",
+                           form = form,
+                           album_music = album,
+                           id=id)
+
 @index_bp.route('/login')
 def login_page():
-    return render_template("auth/login.html",
-                           title = "Login")
+    form = FormLogin()
+    return render_template("auth/login_page.html",
+                           title = "Login",
+                           form=form)
 
+@index_bp.route('/register')
+def register_page():
+    form = FormRegister()
+    return render_template("auth/register_page.html",
+                           title = "Registre-se",
+                           form=form)
